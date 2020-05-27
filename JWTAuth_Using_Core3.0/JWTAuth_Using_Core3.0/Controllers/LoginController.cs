fb55728dfa555356634ca1ba7ec7d9fbfc4c7664
vishal <vishal.verma12914@gmail.com>
2020-05-27 14:34:08 +0530

    JWT Authentication Using Core3.0

---
+++ JWTAuth_Using_Core3.0/JWTAuth_Using_Core3.0/Controllers/LoginController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using JWTAuth_Using_Core3._0.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuth_Using_Core3._0.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IActionResult Login(string userName, string password)
+        {
+            UserModel login = new UserModel
+            {
+                UserName = userName,
+                Password = password
+            };
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+            return response;
+        }
+
+        private string GenerateJSONWebToken(UserModel userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:issuer"],
+                audience: _config["Jwt:issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+                );
+
+            var encodeToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodeToken;
+
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            //For Demo I m using static User Info
+            if(login.UserName == "vk" && login.Password == "soni")
+            {
+                user = new UserModel { UserName = "vishal", Password = "123", Email = "vk@gmail.com" };
+            }
+            return user;
+        }
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claimList = identity.Claims.ToList();
+            var userName = claimList[0].Value;
+            return "Welcome To:" + userName;
+        }
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+    }
+}
---
+++ JWTAuth_Using_Core3.0/JWTAuth_Using_Core3.0/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuth_Using_Core3._0.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
